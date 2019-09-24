108f4f4902585847faa16a437284cda3f967a286
user22 <user22@user22-DL-960GM-GS3-FX>
2019-09-24 14:53:10 +0530

    initial commit by jibin

---
+++ SalesToolV2019/src/com/ust/test/Main.java
@@ -0,0 +1,23 @@
+package com.ust.test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		SalesData objSalesData = new SalesData();
+		
+		System.out.println("Welcome to Sales App");
+		
+		System.out.println("Welcome To Java");
+		
+		displayGreeting();
+
+	}
+	
+	private static void displayGreeting(){
+		
+		System.out.println("Greeting Displayed");
+	}
+
+}
